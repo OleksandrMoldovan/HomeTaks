2fe1134d01f2a2d978a98d6d29f39d68c0656495
0leksandrMoldovan <alexmoldovan1027@gmail.com>
2019-08-12 23:08:46 +0300

    Create HT1.py

---
+++ HT1.py
@@ -0,0 +1,47 @@
+"""The first programm"""
+##a = input("Enter first value:\n")
+##b = input ("Enter second value:\n")
+##sum = print("Sum is:\n", int(a)+int(b))
+##mutlt = print("Multiplication is:\n", int(a)*int(b))
+
+"""The second programm"""
+##a = input("Enter any value\n")
+##sq = int(a)*int(a)
+##print(f"Square of {a} is:\n",sq)
+##print(f"Cub of {a} is:\n",cub)
+
+"""The Third Programm"""
+##a,b,c = input('Value 1:\n'),input('Value 2:\n'),input('Value 3:\n')
+##new_a = (int(a)*2)
+##new_b = (int(b)-3)
+##new_c = (int(c)**2)
+##res = new_a + new_b + new_c
+##print("New sum of values is:",res)
+
+"""The fourth programm"""
+
+##a,b,c = int(input('Value 1:\n')),int(input('Value 2:\n')),int(input('Value 3:\n'))
+##sa = (a+b+c)/3
+##res = (((a+c)*2)-(b*3))
+##print("The result of operation is:",res)
+
+"""The fifth programm"""
+##square_side = int(input("Enter the square side\n"))
+##P = print("The perimeter of square is:",square_side*4)
+##S = print("The square is: ", square_side*square_side)
+
+"""The sixth programm"""
+##cookies = float(input("Enter price of cookies per kg:"))
+##sweets = float(input("Enter price of sweets pre kg:"))
+##firstBuy = round(cookies*0.4+ sweets*0.3, 2)
+##print("First buy is:", firstBuy)
+##secondBuy = round(3*(cookies*2+sweets*1.8),2)
+##print("Second buy is:", secondBuy)
+
+"""The seventh programm"""
+"""
+
+Пользователь вводит цены 1 кг конфет и 1 кг печенья. Найдите стоимость: а) одной покупки из 300 г конфет и 400 г печенья; б) трех покупок, каждая из 2 кг печенья и 1 кг 800 г конфет.
+Пользователь вводит время в минутах и расстояние в километрах. Найдите скорость в м/c.
+Даны катеты прямоугольного треугольника. Найдите площадь, периметр и гипотенузу треугольника.
+Дано значение температуры в градусах Цельсия. Вывести температуру в градусах Фаренгейта."""
