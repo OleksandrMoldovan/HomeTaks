7091a9d19a2043c3fcc97d4c515af5e1e55fb705
Oleksandr <39278615+OleksandrMoldovan@users.noreply.github.com>
2019-08-16 19:52:50 +0300

    Add files via upload

--- HT1.1.py
+++ HT1.1.py
@@ -5,17 +5,17 @@
 ##else: print("Symbols amount is nechetnoe")
 
 """The 2 program"""
-i = True
-num = str([0,2,4,6,8])
-s1 = input("Enter the value\n")
-while i == True:
-    if len(s1)!= 10:
-        print("The string has not 10 symbols")
-        s1 = input("Enter the value\n")
-        if len(s1)== 10:
-            for i in range(0, len(s1)):
-                if i%2 == 0 and s1[i] in num and i+1%2 ==1 and (s1[i].isalpha  s1[i] not in num):
-                    print("String has correct format"))
+##i = True
+##num = str([0,2,4,6,8])
+##s1 = input("Enter the value\n")
+##while i == True:
+##    if len(s1)!= 10:
+##        print("The string has not 10 symbols")
+##        s1 = input("Enter the value\n")
+##        if len(s1)== 10:
+##            for i in range(0, len(s1)):
+##                if i%2 == 0 and s1[i] in num and i+1%2 ==1 and (s1[i].isalpha  s1[i] not in num):
+##                    print("String has correct format"))
 
 
 ##"""The 3 program"""
@@ -85,10 +85,16 @@ while i == True:
 	
 
 """eigth programm""" 
-s = input("Enter symbols\n")
-if s.startswith('abc'):
-    s.replace('abc','www',1)
-    print(s)
+##s = input("Enter the value\n")
+##new_s = list(s)
+##if new_s[0]=="a" and new_s[1]=="b" and new_s[2]=="c":
+##    new_s[0]="w"
+##    new_s[1]="w"
+##    new_s[2]="w"
+##else: new_s.extend(["zzz"])
+##print("".join(new_s))    
+##    
+
 
 """Progr 9"""
 ##s1 = input("Enter the value\n")
